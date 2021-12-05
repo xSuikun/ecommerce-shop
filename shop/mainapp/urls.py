aca1ee2208b35f580c26cc45b9b83e1bfdcec3ce
reaver <antonsharp00@gmail.com>
2021-12-05 03:40:35 +0300

    Revert "Sign In and Sign Up modules and other"
    This reverts commit 6df93590a619dca033c0f9e6e28d479d35d8034f.

--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -16,5 +16,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page="/"), name='logout'),
     path('registration/', RegistrationView.as_view(), name='registration'),
-    path('registration/', RegistrationView.as_view(), name='registration'),
 ]
