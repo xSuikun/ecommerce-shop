77476d4b41285877c12e04de1ead94791941393f
Anton <suykun228@gmail.com>
2021-12-31 08:55:10 +0300

    Почистил код в tests

--- shop/tests/test_api.py
+++ shop/tests/test_api.py
@@ -4,19 +4,14 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 
-from mainapp.models import Product, Category
-from mainapp.serializers import ProductListSerializer, CategoryListSerializer
-from specs.models import ProductFeatures
-
-from mainapp.logic import operations
+from mainapp.models import Category
 
 
 class ShopAPITestCase(TestCase):
     def test_category_serializer(self):
         test_category = Category.objects.create(name='TestCategory', slug='test_category')
-        url = 'http://localhost/api/categories/'
-        print(f'Url: {url}')
-        response = self.client.get(url)
+        url = 'http://localhost/api/categories/?format=json'
+        response = self.client.get(url, format='json')
         expected_data = [OrderedDict([('name', 'TestCategory'), ('slug', 'test_category')])]
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(response.data, expected_data)
